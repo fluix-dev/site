442e62010fd8792c841176af5d2ff64a20303342
TheAvidDev <AvidDev@protonmail.com>
2019-09-13 10:34:07 -0400

    Added ContactMessage model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.CharField(max_length=2047)
+
+    def __str__(self):
+        return self.name
