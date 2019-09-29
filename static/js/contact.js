b7732a1112a49a04d93f4af6ed7d00fca919022a
TheAvidDev <AvidDev@protonmail.com>
2019-09-29 17:56:57 -0400

    Fixed csrf token for contact form ajax

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,3 +1,26 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+// these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
 $(document).ready(function() {
   $('#contact-form').on('submit', function(e){
 
@@ -5,6 +28,15 @@ $(document).ready(function() {
     $('#contact-submit').prop("disabled", true);
     $('#contact-submit').val("Sending...");
     e.preventDefault();
+
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
 
     //Send the form
     $.ajax({
