ad070f37c1bf575ef72c64ea5b5148aa6afca044
TheAvidDev <AvidDev@protonmail.com>
2020-06-10 01:53:49 -0400

    Add django-admin-interface customization

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
