05f2b9e9c284cf4441273ab640cb248d604d4593
TheAvidDev <AvidDev@protonmail.com>
2020-08-03 03:50:29 -0400

    Add metadata and other info to head

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from .models import Blog, Project
+import re
+
+from .models import About, Blog, Project
 from .templatetags.mistune import mistune_html_no_highlight
 
 from django.contrib.syndication.views import Feed
@@ -10,10 +12,38 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 
-class IndexView(ListView):
+class MetadataMixin(object):
+    title = "TheAvidDev"
+    description = "Somwhere on the internet..."
+    og_type = "website"
+
+    def get_title(self):
+        return self.title
+
+    def get_description(self):
+        return self.description
+
+    def get_og_type(self):
+        return self.og_type
+
+    def get_context_data(self, **kwargs):
+        return {
+            "title": self.get_title(),
+            "description": self.get_description(),
+            "og_type": self.get_og_type(),
+            **super().get_context_data(**kwargs)
+        }
+
+
+
+class IndexView(MetadataMixin, ListView):
     model = Blog
     template_name = "index.html"
     context_object_name = "blogs"
+    title = "TheAvidDev's Blog"
+
+    def get_description(self):
+        return About.objects.get().description
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -24,6 +54,8 @@ class IndexView(ListView):
 
 class TimelineView(IndexView):
     template_name = "timeline.html"
+    title = "Blog Timeline"
+    description = "A timeline of all the blogs I've ever written."
 
 
 class RSSBlogFeed(Feed):
@@ -46,10 +78,20 @@ class AtomBlogFeed(RSSBlogFeed):
     subtitle = RSSBlogFeed.description
 
 
-class BlogView(DetailView):
+class BlogView(MetadataMixin, DetailView):
     model = Blog
     template_name = "blog.html"
     context_object_name = "blog"
+    og_title = "article"
+
+    def get_title(self):
+        return self.get_object().title.split("-")[0]
+
+    def get_description(self):
+        # Remove newlines and markup links
+        regex = "[\\[\\]\\n\\r]|\\([^\\(\\)]+\\)"
+        content = self.get_object().content.split("<!--break-->")[0]
+        return re.sub(regex, "", content)
 
     def get_object(self):
         blog = super().get_object()
@@ -58,11 +100,21 @@ class BlogView(DetailView):
         return blog
 
 
-class ProjectsView(ListView):
+class ProjectsView(MetadataMixin, ListView):
     model = Project
     template_name = "projects.html"
     context_object_name = "projects"
+    title = "Project List"
+
+    def get_description(self):
+        return ("A list of the most notable projects I've worked on "
+            "throughout the years. It ranges from websites, to applications, to "
+            "tools, and random scripts.")
 
 
-class AboutView(TemplateView):
+class AboutView(MetadataMixin, TemplateView):
     template_name = "about.html"
+    title = "About Page"
+
+    def get_description(self):
+        return About.objects.get().description
